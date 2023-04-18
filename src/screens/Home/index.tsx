e743703b6b411cb256ffcf800eeb7e9da2416257
Lucas Oliveira <olivmath@protonmail.com>
2023-04-17 22:33:20 -0300

    add: talk away start

---
+++ src/screens/Home/index.tsx
@@ -0,0 +1,10 @@
+import { Text } from "react-native";
+import { Container } from "./styles";
+
+export function Home() {
+  return (
+    <Container>
+      <Text style={{ fontSize: 50 }}>Talk Away!</Text>
+    </Container>
+  );
+}
---
+++ src/screens/theme/index.ts
@@ -0,0 +1,11 @@
+export default {
+    COLORS: {
+        DEEP_OCEAN: '#042940',
+        FOREST_GREEN: '#005C53',
+        SPRING_LEAF: '#9FC131',
+        LEMON_BURST: '#DBF227',
+        KHAKI_MIST: '#D6D58E',
+    },
+  FONTS: {},
+};
+
