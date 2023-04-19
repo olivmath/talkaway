601242c031ee30e29f3c985cec12d27eb49da3ff
Lucas Oliveira <olivmath@protonmail.com>
2023-04-18 23:43:30 -0300

    add(routes): routes are home, walking, maketplace, and perfil

---
+++ src/routes/app.router.tsx
@@ -0,0 +1,54 @@
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import { Home } from "../screens/Home";
+import { Profile } from "../screens/Profile";
+import { Search } from "../screens/Search";
+import { Marketplace } from "../screens/Marketplace";
+import { Feather } from "@expo/vector-icons";
+
+const { Navigator, Screen } = createBottomTabNavigator();
+
+export function AppRoutes() {
+  return (
+    <NavigationContainer>
+      <Navigator>
+        <Screen
+          name="Home"
+          component={Home}
+          options={{
+            tabBarIcon: () => {
+              return <Feather name="home" size={25} color="#000" />;
+            },
+          }}
+        />
+        <Screen
+          name="Walking"
+          component={Search}
+          options={{
+            tabBarIcon: () => {
+              return <Feather name="map" size={25} color="#000" />;
+            },
+          }}
+        />
+        <Screen
+          name="Marketplace"
+          component={Marketplace}
+          options={{
+            tabBarIcon: () => {
+              return <Feather name="shopping-cart" size={25} color="#000" />;
+            },
+          }}
+        />
+        <Screen
+          name="Profile"
+          component={Profile}
+          options={{
+            tabBarIcon: () => {
+              return <Feather name="user" size={25} color="#000" />;
+            },
+          }}
+        />
+      </Navigator>
+    </NavigationContainer>
+  );
+}
