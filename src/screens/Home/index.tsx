803256bc24b54222c270cf5d0c039d995f95eb5f
Lucas Oliveira <olivmath@protonmail.com>
2023-04-20 15:12:24 -0300

    remove: wrong search bar

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,35 +1,9 @@
-import React, { useState } from "react";
-import { View, Text, TouchableOpacity, TextInput, Keyboard } from "react-native";
-import { Feather } from "@expo/vector-icons";
+import { View, Text } from "react-native";
 import { styles } from "./styles";
 
 export function Home() {
-  const [searchOpen, setSearchOpen] = useState(false);
-
-  const handleSearchIconPress = () => {
-    setSearchOpen(true);
-  };
-
-  const handleCloseSearch = () => {
-    setSearchOpen(false);
-  };
-
   return (
     <View style={styles.container}>
-      <TouchableOpacity onPress={handleSearchIconPress} style={styles.searchIcon}>
-        <Feather name="search" size={24} color="black" />
-      </TouchableOpacity>
-
-      {searchOpen && (
-        <View style={styles.searchContainer}>
-          <TextInput
-            style={styles.searchInput}
-            autoFocus={true}
-            placeholder="Search..."
-            onBlur={handleCloseSearch}
-          />
-        </View>
-      )}
 
       <Text style={styles.text}>Talk Away!</Text>
     </View>
--- src/screens/Home/styles.ts
+++ src/screens/Home/styles.ts
@@ -10,32 +10,4 @@ export const styles = StyleSheet.create({
     fontSize: 50,
     fontFamily: "ROBOTO_LIGHT",
   },
-  searchIcon: {
-    position: "absolute",
-    top: 20,
-    right: 20,
-    zIndex: 1,
-  },
-  searchIconText: {
-    fontSize: 30,
-  },
-  searchContainer: {
-    position: "absolute",
-    top: 0,
-    left: 0,
-    right: 0,
-    bottom: 0,
-    backgroundColor: "white",
-    zIndex: 2,
-    paddingHorizontal: 20,
-    paddingTop: 50,
-  },
-  searchInput: {
-    borderWidth: 1,
-    borderColor: "#ccc",
-    borderRadius: 10,
-    paddingHorizontal: 10,
-    paddingVertical: 5,
-    fontSize: 20,
-  },
 });
