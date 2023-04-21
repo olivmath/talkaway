ec4920e74d4b076779f1467132b88cd3aa403672
Lucas Oliveira <olivmath@protonmail.com>
2023-04-20 21:28:47 -0300

    update: colors

--- src/theme/index.ts
+++ src/theme/index.ts
@@ -1,15 +1,13 @@
 export default {
-    COLORS: {
-        DEEP_OCEAN: '#042940',
-        FOREST_GREEN: '#005C53',
-        SPRING_LEAF: '#9FC131',
-        LEMON_BURST: '#DBF227',
-        KHAKI_MIST: '#D6D58E',
-    },
+  COLORS: {
+    TANGERINE: "#FF9D48",
+    GOLDEN: "#FFCD6C",
+    CRIMSON: "#B61430",
+    INDIGO: "#177D8F",
+  },
   FONTS: {
-    REGULAR: 'Roboto_400Regular',
-    MEDIUM: 'Roboto_500Medium',
-    BOLD: 'Roboto_700Bold',
+    REGULAR: "Roboto_400Regular",
+    MEDIUM: "Roboto_500Medium",
+    BOLD: "Roboto_700Bold",
   },
 };
-
