2b9048188e74e35d7741e67a4051ea21bffa56ac
Lucas Oliveira <olivmath@protonmail.com>
2023-04-23 23:27:39 -0300

    add(marketplace): cards into marketplace main component

--- src/screens/Marketplace/index.tsx
+++ src/screens/Marketplace/index.tsx
@@ -1,10 +1,21 @@
-import { View, Text } from "react-native";
+import React from "react";
+import { ScrollView, View } from "react-native";
+import { WalkingClassCard } from "./WalkingClassCard";
 import { styles } from "./styles";
+import db from "./db";
 
 export function Marketplace() {
   return (
-    <View style={styles.container}>
-      <Text style={styles.text}>Marketplace!</Text>
-    </View>
+    <ScrollView style={styles.container}>
+      <View style={styles.list}>
+        {db.map((classData, index) => (
+          <WalkingClassCard
+            key={index}
+            {...classData}
+            onPress={() => console.log(`Aula selecionada: ${index}`)}
+          />
+        ))}
+      </View>
+    </ScrollView>
   );
 }
