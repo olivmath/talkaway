a218806a87b7b22f6456aba6a7c099173e8ac70e
Lucas Oliveira <olivmath@protonmail.com>
2023-04-23 23:27:39 -0300

    add(marketplace): cards into marketplace main component

--- src/screens/Marketplace/styles.ts
+++ src/screens/Marketplace/styles.ts
@@ -4,11 +4,9 @@ import theme from "../../theme";
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
   },
-  text: {
-    fontSize: 50,
-    fontFamily: theme.FONTS.OS_MD,
+  list: {
+    paddingTop: 24,
+    paddingHorizontal: 24,
   },
 });
